edabe1c3629dbcbadbd651b803aa2beb00718b01
杨安府 <565870260@qq.com>
2021-09-27 10:51:33 +0800

    :white_check_mark: format

--- src/icon/components/failure.tsx
+++ src/icon/components/failure.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default ({ color = '#202020', size = 14 }) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns='http://www.w3.org/2000/svg'
     height={size}
     width={size}
-    viewBox="0 0 3.44 2.728"
+    viewBox='0 0 3.44 2.728'
   >
     <path
-      d="M2.12 1.377l.961.962-.379.38-.964-.963-.962.963-.38-.38.963-.962L.396.413l.38-.38.962.963.964-.962.38.38z"
+      d='M2.12 1.377l.961.962-.379.38-.964-.963-.962.963-.38-.38.963-.962L.396.413l.38-.38.962.963.964-.962.38.38z'
       fill={color}
     />
   </svg>
--- src/icon/components/success.tsx
+++ src/icon/components/success.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default ({ color = '#202020', size = 14 }) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 3.44 2.728"
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 3.44 2.728'
     height={size}
     width={size}
   >
     <path
-      d="M3.37.473L1.187 2.654.098 1.562l.409-.409.68.682L2.96.063z"
+      d='M3.37.473L1.187 2.654.098 1.562l.409-.409.68.682L2.96.063z'
       fill={color}
     />
   </svg>
